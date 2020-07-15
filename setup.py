8d267c98dcb944dfd380bfbf99d720e8c71b7c3c
mapicccy <mapicccy@gmail.com>
2020-07-15 06:24:58 +0000

    bugfix: fix AttributeError when pip>=20.0
    Signed-off-by: mapicccy <mapicccy@gmail.com>

--- setup.py
+++ setup.py
@@ -9,10 +9,14 @@ from setuptools import (
 )
 
 try: # for pip >= 10
-        from pip._internal.req import parse_requirements
+    from pip._internal.req import parse_requirements
 except ImportError: # for pip <= 9.0.3
-        from pip.req import parse_requirements
+    from pip.req import parse_requirements
 
+try:
+    req = [str(ir.req) for ir in parse_requirements("requirements.txt", session=False)]
+except:
+    req = [str(ir.requirement) for ir in parse_requirements("requirements.txt", session=False)]
 
 with open(join(dirname(__file__), 'funcat/VERSION.txt'), 'rb') as f:
     version = f.read().decode('ascii').strip()
