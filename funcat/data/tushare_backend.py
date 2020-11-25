80eddd515bbc03e5e1c98cd3b97ff050f729719b
mapicccy <mapicccy@gmail.com>
2020-11-25 02:51:22 +0000

    get stock_basics using tushare pro
    Signed-off-by: mapicccy <mapicccy@gmail.com>

--- funcat/data/tushare_backend.py
+++ funcat/data/tushare_backend.py
@@ -28,7 +28,7 @@ class TushareDataBackend(DataBackend):
 
     @cached_property
     def stock_basics(self):
-        return self.ts.get_stock_basics()
+        return self.ts.pro_api().stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date').set_index('symbol', drop=True)
 
     @cached_property
     def code_name_map(self):
