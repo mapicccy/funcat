5b44cd4bb217390909444f568b351fa200e250e1
Guanjun <guanjun@linux.alibaba.com>
2022-07-28 15:08:33 +0000

    tushare: fix dataframe NoneType error
    We encounter dataframe NoneType error when specific ts_code is new on
    the market. the stack are as follows:
    
    Traceback (most recent call last):
      File "/root/project/stock/choose_stock.py", line 294, in <module>
        callback=callback,
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/helper.py", line 18, in wrapper
        return func(*args, **kwargs)
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/helper.py", line 54, in select
        choose(order_book_id, func, callback)
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/helper.py", line 27, in choose
        if func():
      File "/root/project/stock/choose_stock.py", line 291, in <lambda>
        lambda: select_over_average(31) and select_long_average_up(5) and select_down_from_max(31, 1.12) and HHV(H, 21) / C > 1.12,
      File "/root/project/stock/choose_stock.py", line 72, in select_over_average
        ma5 = MA(C[i], 5)
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/time_series.py", line 271, in __getitem__
        return self.__class__(series=self.series[:len(self.series) - index], **self.extra_create_kwargs)
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/time_series.py", line 275, in series
        self._ensure_series_update()
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/time_series.py", line 251, in _ensure_series_update
        bars = get_bars(freq)
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/time_series.py", line 20, in get_bars
        bars = data_backend.get_price(order_book_id, start=start_date, end=current_date, freq=freq)
      File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/data/tushare_backend.py", line 140, in get_price
        df["datetime"] = df["trade_date"].apply(lambda x: int(x.replace("-", "")) * 1000000)
    TypeError: 'NoneType' object is not subscriptable
    
    Under this situation, everything is meaningless, just return np.array([]) when get_price.
    
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>

--- funcat/data/tushare_backend.py
+++ funcat/data/tushare_backend.py
@@ -129,7 +129,12 @@ class TushareDataBackend(DataBackend):
                 else:
                     df = self.ts.pro_bar(ts_code=order_book_id, adj='qfq', start_date=str_start_date, end_date=str_end_date)
             except IOError:
+                # tushare has some stricts, just sleep 1 min when encounter IOError.
                 time.sleep(60)
+                return np.array([])
+
+            # A newly order_book_id maybe None here
+            if df is None:
                 return np.array([])
 
             if freq[-1] == "m":
