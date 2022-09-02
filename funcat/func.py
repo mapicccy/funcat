mapicccy/funcat

commit 85b729a8d933514d7b65ae01c29c4333d1efdfe3

talib: support higher TA-Lib versions.
Higher version TA-Lib may zip the (self, series, arg)
into *args, use class function to avoid passing the
argument 'self' downstream.

After this patch, I verify all the python version and
TA-Lib version. All works fine with me.

Glad to see this. Maybe we could take a further step
to release a official version for the repo

Signed-off-by: Guanjun <guanjun@linux.alibaba.com>

funcat/func.py
@@ -32,7 +32,12 @@ class OneArgumentSeries(NumericSeries):
 
             try:
                 series[series == np.inf] = np.nan
-                series = self.func(series, arg)
+
+                """
+                Higher version TA-Lib may zip the (self, series, arg) into *args,
+                use class function here to avoid passing self downstream.
+                """
+                series = self.__class__.func(series, arg)
             except Exception as e:
                 raise FormulaException(e)
         super(OneArgumentSeries, self).__init__(series)
@@ -67,7 +72,7 @@ class TwoArgumentSeries(NumericSeries):
 
             try:
                 series[series == np.inf] = np.nan
-                series = self.func(series, arg1, arg2)
+                series = self.__class__.func(series, arg1, arg2)
             except Exception as e:
                 raise FormulaException(e)
         super(TwoArgumentSeries, self).__init__(series)
@@ -99,7 +104,7 @@ class CCISeries(NumericSeries):
                 series0[series0 == np.inf] = np.nan
                 series1[series1 == np.inf] = np.nan
                 series2[series2 == np.inf] = np.nan
-                series = self.func(series0, series1, series2)
+                series = self.__class__.func(series0, series1, series2)
             except Exception as e:
                 raise FormulaException(e)
             super(CCISeries, self).__init__(series)

commit 82be853438da4fb6f32a4ce5cbc601c92564b357

func: bugfix for missing 1 required positional argument
When function type switching from instance to class, The SMASeries
self defined function should also be changed.

Otherwise, it will encounter this issue when invoke SMA(C, arg1, arg2).

  Traceback (most recent call last):
    File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/func.py", line 75, in __init__
      series = self.__class__.func(series, arg1, arg2)
  TypeError: func() missing 1 required positional argument: '_'

  During handling of the above exception, another exception occurred:

  Traceback (most recent call last):
    File "test.py", line 65, in <module>
      if C<REF(C,1) and ZJFZ()>=0.1 and CCI()[-1]<=-110:
    File "test.py", line 27, in ZJFZ
      VAR3=SMA(ABS(L-VAR2),3,1)/SMA(MAX(L-VAR2,0),3,1)*100
    File "/root/miniconda3/envs/py36/lib/python3.6/site-packages/funcat-1.1.0-py3.6.egg/funcat/func.py", line 77, in __init__
      raise FormulaException(e)
  funcat.utils.FormulaException: func() missing 1 required positional argument: '_'

Fixes: 85b729a8d933 (talib: support higher TA-Lib versions.)
Signed-off-by: Guanjun <guanjun@linux.alibaba.com>

funcat/func.py
@@ -83,7 +83,7 @@ class TwoArgumentSeries(NumericSeries):
 class SMASeries(TwoArgumentSeries):
     """同花顺专用SMA"""
 
-    def func(self, series, n, _):
+    def func(series, n, _):
         results = np.nan_to_num(series).copy()
         # FIXME this is very slow
         for i in range(1, len(series)):